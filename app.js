1653c2bc7baf43f9b655d956d89424f3180fc250
Kelsey Brisson <klbrisson@gmail.com>
2014-05-09 13:00:21 -0600

    Initial Commit. 3d card opens on hover.

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var port = process.env.PORT || 6824;
+
+
+var server = app.listen(6824, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
